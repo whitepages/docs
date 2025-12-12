bf748ac4de0591e7e866e0bf82ee01b53a9e9026
Kevin Edry <kedry@whitepages.com>
2025-12-12 10:24:19 -0800

    fix: update ESLint config for Next.js 16 flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  globalIgnores([
+    "node_modules/**",
+    ".next/**",
+    ".open-next/**",
+    "out/**",
+    "build/**",
+    ".source/**",
+    "next-env.d.ts",
+  ]),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      ".source/**",
-      "next-env.d.ts",
-    ],
+    rules: {
+      "import/no-anonymous-default-export": "off",
+      "react-hooks/refs": "off",
+      "react-hooks/immutability": "off",
+      "react-hooks/set-state-in-effect": "off",
+    },
   },
-];
+]);
 
 export default eslintConfig;
